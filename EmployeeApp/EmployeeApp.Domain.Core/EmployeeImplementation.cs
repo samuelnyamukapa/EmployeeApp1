4371fddf57e172e32557c220ee0b156648747737
samuelnyamukapa <samuelnyamukapa@yahoo.com>
2013-08-11 22:52:40 +0100

    Domain logic  starts

--- EmployeeApp/EmployeeApp.Domain.Core/EmployeeImplementation.cs
+++ EmployeeApp/EmployeeApp.Domain.Core/EmployeeImplementation.cs
@@ -5,10 +5,11 @@ using System.Text;
 using EmployeeApp.Data;
 using EmployeeApp.Data.Repositories;
 using EmployeeApp.Model;
+using EmployeeApp.Domain.Core.Interfaces;
 
 namespace EmployeeApp.Domain.Core
 {
-    public class EmployeeImplementation
+    public class EmployeeImplementation : IEmployeeImplementation
     {
         IEmployee _repository;
         public EmployeeImplementation(IEmployee repository)
@@ -16,5 +17,40 @@ namespace EmployeeApp.Domain.Core
             _repository = repository;
         }
 
+
+        public EmployeeModel Add(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(EmployeeModel entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeModel GetByEmployeeCode(int employeeCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeModel GetByEmployeeName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeModel> GetAllEmployeesByDate(DateTime startDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeModel> GetAllEmployeese()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ EmployeeApp/EmployeeApp.Domain.Core/Interfaces/IEmployeeImplementation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EmployeeApp.Model;
+
+namespace EmployeeApp.Domain.Core.Interfaces
+{
+    public interface IEmployeeImplementation
+    {
+        EmployeeModel Add(EmployeeModel entity);
+        bool Update(EmployeeModel entity);
+        bool Delete(EmployeeModel entity);
+        EmployeeModel GetByEmployeeCode(int employeeCode);
+        EmployeeModel GetByEmployeeName(string name);
+        List<EmployeeModel> GetAllEmployeesByDate(DateTime startDate);
+        List<EmployeeModel> GetAllEmployeese();
+    }
+}
